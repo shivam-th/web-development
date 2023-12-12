1efe76f7819b92d24782dfa5d0f7a31dff0f855d
shivam-th <shivamthakur0567@gmail.com>
2023-12-13 04:47:46 +0530

    jdhdd

---
+++ amazon-clone/client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header></header>
+      cc
+      <div className="a-main"></div>
+
+      <footer></footer>
+
+      
+
+    </>
+
+  );
+}
+
+export default App;
